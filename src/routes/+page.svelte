9c3de6fc33f2a4f5a24e336c73b25aec668fd0bb
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-12-26 05:41:39 +0700

    feat: create welcome page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="home">
+  <div>
+    <h1>Selamat datang di Devcode!</h1>
+    <h2>
+      Silahkan klik tombol di bawah ini untuk melihat hasil akhir dari perubahan
+      yang kamu lakukan.
+    </h2>
+    <div data-cy="router-button">
+      <a class="router__btn" href="/hello-world">Lihat hasil</a>
+    </div>
+  </div>
+</div>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,500;0,700;1,300;1,500;1,700&display=swap");
+
+.home {
+  margin: 15% 0 0;
+  text-align: center;
+
+  font-family: "Poppins", sans-serif;
+}
+.router__btn {
+  padding: 12px;
+  background: aqua;
+  color: #2c3e50;
+  text-decoration-line: none;
+  border-radius: 8px;
+}
+</style>
