53c4b34ac21e4194a6081cf0e8ddf56843190b45
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-12-26 10:46:03 +0700

    feat: create route button to contacts page

--- src/components/WelcomeCard.svelte
+++ src/components/WelcomeCard.svelte
@@ -4,14 +4,13 @@
     Silahkan klik tombol di bawah ini untuk melihat hasil akhir dari perubahan
     yang kamu lakukan.
   </h2>
-  <!-- TODO: Uncomment baris kode dibawah ini untuk menambahkan routing baru ke Halaman Hello -->
-  <!-- <div data-cy="router-button">
+  <div data-cy="router-button">
     <a class="router__btn" href="/hello">Lihat hasil</a>
-  </div> -->
+  </div>
 </div>
 
 <style>
-  .router__btn {
+  :global(.router__btn) {
     padding: 12px;
     background: aqua;
     color: #2c3e50;
---
+++ src/routes/contacts/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Contact Manager Page</h1>
