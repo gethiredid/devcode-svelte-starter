b0fb76ce862718b3555aefb82ea8d6a364894eb0
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-12-26 09:40:42 +0700

    feat: create welcome card component

---
+++ src/components/WelcomeCard.svelte
@@ -0,0 +1,20 @@
+<div>
+  <h1>Selamat datang di Devcode!</h1>
+  <h2>
+    Silahkan klik tombol di bawah ini untuk melihat hasil akhir dari perubahan
+    yang kamu lakukan.
+  </h2>
+  <div data-cy="router-button">
+    <a class="router__btn" href="/hello-world">Lihat hasil</a>
+  </div>
+</div>
+
+<style>
+.router__btn {
+  padding: 12px;
+  background: aqua;
+  color: #2c3e50;
+  text-decoration-line: none;
+  border-radius: 8px;
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,20 @@
+<script>
+  import WelcomeCard from "../components/WelcomeCard.svelte";
+</script>
+
 <div class="home">
-  <div>
-    <h1>Selamat datang di Devcode!</h1>
-    <h2>
-      Silahkan klik tombol di bawah ini untuk melihat hasil akhir dari perubahan
-      yang kamu lakukan.
-    </h2>
-    <div data-cy="router-button">
-      <a class="router__btn" href="/hello-world">Lihat hasil</a>
-    </div>
-  </div>
+  <WelcomeCard />
 </div>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,500;0,700;1,300;1,500;1,700&display=swap");
 
+:global(body){
+  font-family: "Poppins", sans-serif;
+}
+
 .home {
   margin: 15% 0 0;
   text-align: center;
-
-  font-family: "Poppins", sans-serif;
-}
-.router__btn {
-  padding: 12px;
-  background: aqua;
-  color: #2c3e50;
-  text-decoration-line: none;
-  border-radius: 8px;
 }
 </style>
