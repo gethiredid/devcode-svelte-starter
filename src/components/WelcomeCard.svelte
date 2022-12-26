a0e7d75aab4d7e79f466c5369d64626792f2f49a
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-12-26 10:27:33 +0700

    feat: add new todo setup routing to hello page

--- src/components/WelcomeCard.svelte
+++ src/components/WelcomeCard.svelte
@@ -4,9 +4,10 @@
     Silahkan klik tombol di bawah ini untuk melihat hasil akhir dari perubahan
     yang kamu lakukan.
   </h2>
-  <div data-cy="router-button">
+  <!-- TODO: Uncomment baris kode dibawah ini untuk menambahkan routing baru ke Halaman Hello -->
+  <!-- <div data-cy="router-button">
     <a class="router__btn" href="/hello">Lihat hasil</a>
-  </div>
+  </div> -->
 </div>
 
 <style>
